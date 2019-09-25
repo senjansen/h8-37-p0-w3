c569750ea34d7b33449e12b2d88d983f81d6f3d0
senjansen <senjansen.sj@gmail.com>
2019-09-25 19:11:45 +0700

    add exercise 5

---
+++ exercise-5.js
@@ -0,0 +1,22 @@
+// Jansen - Hacktiv8 Batch 37 - King Fox
+// Exercise 5 - Palindrome
+
+// Function to return true if the word is a palindrome
+function palindrome(kata) {
+    let reverse = '';
+    let kataLength = kata.length;
+
+    for (let i = 1; i <= kataLength; i++) {
+        reverse += kata[kataLength - i];
+    }
+    
+    return kata === reverse;
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
+
